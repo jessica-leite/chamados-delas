9e1cfad814f6f5a01307fa52e9483e56590e8e16
Jessica Leite <jessica.mleite@gmail.com>
2024-05-03 17:35:36 -0300

    Arquivos iniciais

---
+++ src/main/java/com/api/chamadosdelas/ChamadosdelasApplication.java
@@ -0,0 +1,13 @@
+package com.api.chamadosdelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadosdelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadosdelasApplication.class, args);
+	}
+
+}
