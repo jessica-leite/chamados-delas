fb3b4c1ed80427fdcf390c6190126ea5cfc35077
alyce <aaquegi@hotmail.com>
2024-05-07 21:42:26 -0300

    CRUD Setor

---
+++ src/main/java/com/api/chamadosdelas/controllers/SetorController.java
@@ -0,0 +1,40 @@
+package com.api.chamadosdelas.controllers;
+
+
+import com.api.chamadosdelas.models.Setor;
+import com.api.chamadosdelas.services.SetorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class SetorController {
+    @Autowired
+    private SetorService setorService;
+
+    @RequestMapping(value = "/cadsetor", method = RequestMethod.POST)
+    public Setor save(@RequestBody Setor c) {
+        return this.setorService.save(c);
+    }
+
+    @RequestMapping(value = "/mostrarsetores", method = RequestMethod.GET)
+    public List<Setor> findAll() {
+        return this.setorService.findAll();
+    }
+
+    @RequestMapping(value = "/mostrarsetor/{id}", method = RequestMethod.GET)
+    public Setor findById(@PathVariable int id) {
+        return this.setorService.findById(id);
+    }
+
+    @RequestMapping(value = "/excluirsetor/{id}", method = RequestMethod.DELETE)
+    public String deleteSetor(@PathVariable("id") int id) {
+        return this.setorService.deleteById(id);
+    }
+
+    @RequestMapping(value = "/alterarsetor/{id}", method = RequestMethod.PUT)
+    public Setor updateById(@PathVariable int id, @RequestBody Setor c) {
+        return this.setorService.updateById(id, c);
+    }
+}
---
+++ src/main/java/com/api/chamadosdelas/repositories/SetorRepository.java
@@ -0,0 +1,7 @@
+package com.api.chamadosdelas.repositories;
+
+import com.api.chamadosdelas.models.Setor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SetorRepository extends JpaRepository<Setor, Integer> {
+}
