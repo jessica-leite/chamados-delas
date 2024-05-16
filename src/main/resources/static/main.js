e6065cdb16be5fc12513f72ed54236acf48d04ea
Julia Domingues <142114663+julooch@users.noreply.github.com>
2024-05-15 22:16:18 -0300

    feat: finalização tela-adm

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -17,7 +17,7 @@ function getChamadosUsuario(id){
     var data = new Date(chamados[i].dataInicio);
     var dataFormatada = data.getDate() + '-' + (data.getMonth() + 1) + '-' + data.getFullYear();
     var rotaEditarChamado = "atualizar-chamado";
-        linhas += '<tr id="'+chamados[i].id+'" class="font-['+'Quicksand'+'] items-center no-wrap mx-5 text-xs mt-5 px-5 gap-12 flex justify-between text-[#B9375E] bg-[#FFE0E9] rounded-md h-8 w-8/8">'
+        linhas += '<tr id="'+chamados[i].id+'" class="font-['+'Quicksand'+'] items-center no-wrap mb-3 mx-5 text-xs mt-5 px-5 gap-12 flex justify-between text-[#B9375E] bg-[#FFE0E9] rounded-md h-8 w-8/8">'
                         + '<td><button onclick="excluirChamado('+ chamados[i].id +')" class="font-['+'Quicksand'+'] font-bold mr-5 bg-[#800E13] hover:bg-[#38040E] active:bg-[#800E13] focus:outline-none focus:ring rounded-md text-white py-1 px-2 mt-1">'+'X'+'</button></td>'
                         +'<td><button OnClick=" location.href=' + "'" + rotaEditarChamado + "'" +'" class=" font-['+'Quicksand'+'] bg-green-700 hover:bg-green-900 active:bg-green-800 focus:outline-none focus:ring text-1xl mt-1 rounded-md p-1 px-2 font-bold text-white">'+'EDITAR'+'</button></td>'
                         + '<td>'+ chamados[i].titulo + '</td>'
@@ -102,7 +102,7 @@ var chamados = request( "GET", "mostrarchamados");
     var data = new Date(chamados[i].dataInicio);
     var dataFormatada = data.getDate() + '-' + (data.getMonth() + 1) + '-' + data.getFullYear();
 
-        linhas += ' <tr class="font-['+'Quicksand'+'] items-center no-wrap mx-5 text-xs mt-5 px-5 gap-12 flex justify-between text-[#B9375E] bg-[#FFE0E9] rounded-md h-8 w-8/8">'
+        linhas += ' <tr class="font-['+'Quicksand'+'] items-center mb-3 no-wrap mx-5 text-xs mt-5 px-5 gap-12 flex justify-between text-[#B9375E] bg-[#FFE0E9] rounded-md h-8 w-8/8">'
                                                      + '<td>' + chamados[i].id + '</td>'
                                                      +'<td class="truncate">'+ chamados[i].titulo + '</td>'
                                                      +'<td>'
